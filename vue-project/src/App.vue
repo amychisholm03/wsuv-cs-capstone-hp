3bb67c7ec6607494c510ea52adeb6b1f2e4b4ac4
Gabrielle-Barling <156250200+Gabrielle-Barling@users.noreply.github.com>
2024-10-24 13:50:25 -0700

    feat: display API call served from UI server to back end (#5)

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,37 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 const message = ref('')
 const jobTitle = ref('')
 const pageCount = ref('')
 const rasterizationProfile = ref('')
 
 
-const createJob = () => {
+const createJob = async () => {
   if (jobTitle.value.trim() !== '' && pageCount.value.trim() !== '' && rasterizationProfile.value.trim() !== ''){
-    message.value = jobTitle.value + " has been created"
-    setTimeout(() => {
-      message.value = ''
-      jobTitle.value = ''
-      pageCount.value = ''
-      rasterizationProfile.value = ''
-      }, 3000);
+    const url = "http://54.200.253.84:80/createJob"
+    const data = {
+      Title: jobTitle.value.toString(),
+      PageCount: pageCount.value.toString(),
+      RasterizationProfile: rasterizationProfile.value.toString()
+    }
+    const response = await fetch(url, {
+          method: 'POST',
+          mode: 'cors',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(data)
+    })
+    if (!response.ok) {
+      console.log("Error fetching data")
+      console.log("Response from server: " + response)
+    } else {
+      message.value = jobTitle.value + " has been created"
+      setTimeout(() => {
+        message.value = ''
+        jobTitle.value = ''
+        pageCount.value = ''
+        rasterizationProfile.value = ''
+        }, 3000);
+    }
   } else {
     message.value = "Job title, page count, and rasterization profile cannot be left blank"
   }
@@ -22,7 +39,7 @@ const createJob = () => {
 </script>
 
 <template>
-  <h1>Create a New Print Job</h1>
+  <h1>Create New Print Job</h1>
   <form @submit.prevent="createJob">
         <label for="jobTitle">Job Title </label>
         <input type="text" id="jobTitle" name="jobTitle" v-model="jobTitle" />
@@ -35,30 +52,3 @@ const createJob = () => {
   <p>{{ message }}</p>
 </template>
 
-
-
-<!-- 
-  const url = "http://54.200.253.84:80/createJob"
-  const data = {
-    Title: jobTitle.value,
-    PageCount: pageCount.value,
-    RasterizationProfile: [rasterizationProfile.value]
-  }
-  fetch(url, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('There was a problem with the fetch operation:', error);
-      })
--->
