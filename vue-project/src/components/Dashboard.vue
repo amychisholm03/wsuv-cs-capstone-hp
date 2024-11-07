1752864e0c40c98215f1231357eb1df15178e547
Gabrielle-Barling <156250200+Gabrielle-Barling@users.noreply.github.com>
2024-11-07 08:24:44 -0800

    feat: adding images to dashboard (#13)

--- vue-project/src/components/Dashboard.vue
+++ vue-project/src/components/Dashboard.vue
@@ -25,23 +25,23 @@
     <v-main>
       <v-container class="pa-3 ma-3">
         <v-row>
-            <v-card class="ma-3 dashboard-component">
-              <v-toolbar class="pa-2">Component 1</v-toolbar>
-            </v-card>
+          <v-card class="ma-3 dashboard-component" style="width: 425px; height: 305px;">
+            <v-img src="/src/assets/image1.png"></v-img>
+          </v-card>
 
-            <v-card class="ma-3 dashboard-component" >
-              <v-toolbar class="pa-2">Component 2</v-toolbar>
-            </v-card>
+          <v-card class="ma-3 dashboard-component" style="width: 425px; height: 305px;">
+            <v-img src="/src/assets/image2.png"></v-img>
+          </v-card>
         </v-row>
 
         <v-row>
-            <v-card class="ma-3 dashboard-component">
-              <v-toolbar class="pa-2">Component 3</v-toolbar>
-            </v-card>
+          <v-card class="ma-3 dashboard-component" style="width: 425px; height: 305px;">
+            <v-img src="/src/assets/image4.png"></v-img>
+          </v-card>
 
-            <v-card class="ma-3 dashboard-component" >
-              <v-toolbar class="pa-2">Component 4</v-toolbar>
-            </v-card>
+          <v-card class="ma-3 dashboard-component" style="width: 425px; height: 305px;">
+            <v-img src="/src/assets/image3.png"></v-img>
+          </v-card>
         </v-row>
       </v-container>
     </v-main>
@@ -49,16 +49,75 @@
 </template>
 
 <script setup>
-  import { ref, onMounted } from "vue";
-  import { useRouter } from 'vue-router';
+import { ref, onMounted } from "vue";
+import { useRouter } from 'vue-router';
+const router = useRouter();
 
-  const drawer = ref(false);
-  const router = useRouter();
+const message = ref('');
+const drawer = ref(false);
+const jobTitle = ref('');
+const pageCount = ref('');
+const rasterizationProfile = ref('');
+const selectedWorkflow = ref(null);
+const workflows = ref([
+  { id: 1, name: 'Default Workflow' }
+]);
 
-  //// ROUTING ////
-  const routeTo = (where) => {
-    router.push(where);
-  };
+
+drawer.value = false;
+
+//// ROUTING ////
+const routeTo = (where) => {
+  router.push(where);
+};
+
+
+//// API CALLS ////
+
+const createJob = async () => {
+  if (jobTitle.value.trim() !== '' && pageCount.value.trim() !== '' && rasterizationProfile.value.trim() !== '') {
+    const url = "http://54.200.253.84:80/createJob";
+    const data = {
+      Title: jobTitle.value.toString(),
+      PageCount: pageCount.value.toString(),
+      RasterizationProfile: rasterizationProfile.value.toString()
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+      console.log("Error fetching data")
+      console.log("Response from server: " + response)
+    } else {
+      message.value = jobTitle.value + " has been created"
+      setTimeout(() => {
+        message.value = '';
+        jobTitle.value = '';
+        pageCount.value = '';
+        rasterizationProfile.value = '';
+      }, 3000);
+    }
+  } else {
+    message.value = "Job title, page count, and rasterization profile cannot be left blank";
+  }
+}
+
+
+//// OTHER FUNCTIONS ////
+const selectWorkflow = async () => {
+  // once API is made for getting default workflow this will send workflow to backend
+  if (selectedWorkflow.value) {
+    message.value = "Workflow: " + selectedWorkflow.value.name + " has been selected";
+  } else {
+    message.value = "Please select a workflow";
+  }
+}
+
 </script>
 
 <style>
